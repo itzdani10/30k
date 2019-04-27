a6e7f4ca4598fa7e71b18e80511bf7572e00dd69
itzdani10 <50013023+itzdani10@users.noreply.github.com>
2019-04-27 20:31:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,7 +41,7 @@ client.on("message", message => {
 
 client.on('message', message => {
     var  user = message.mentions.users.first() || message.author;
-if (message.content.startsWith("$avatar")) {
+if (message.content.startsWith("%avatar")) {
 message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
 }
 });
